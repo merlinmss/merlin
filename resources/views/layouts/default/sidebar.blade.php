6bcd73abd307958bdc71a1c5f08433cf794cd549
merlinmss <merlin@fortigrid.com>
2026-05-01 21:43:18 +0530

    Test commit1

--- resources/views/layouts/default/sidebar.blade.php
+++ resources/views/layouts/default/sidebar.blade.php
@@ -23,12 +23,19 @@
               </div>
             </li>
 
+            
             <li class="nav-item">
-              <a class="nav-link" href="#">
-                <i class="mdi mdi-grid-large menu-icon"></i>
+              <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+                <i class="menu-icon mdi mdi-account-circle-outline"></i>
                 <span class="menu-title">Settings</span>
+                <i class="menu-arrow"></i>
               </a>
+              <div class="collapse" id="settings">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="#"> General Settings </a></li>
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> Site Settings </a></li>
+                </ul>
+              </div>
             </li>
-
           </ul>
         </nav>
